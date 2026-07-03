b78506d490415a2fabeb1cecb0b185c31520949a
LoganZ2 <1540961836@qq.com>
2026-07-03 13:42:10 +0800

    fix favicon asset path

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head><link rel="icon" href={`${base}/favicon.svg`} /></svelte:head>
 {@render children()}
